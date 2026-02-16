4f0ad7d130c492912642549c0b0d51de93bf70cc
omaaarsh <omarsherifelghamry@gmail.com>
2026-02-16 03:27:26 +0200

    fix-ci-file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
 // src/main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
