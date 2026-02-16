d288faaf8280c4856ba4b63dfa0e0927c14886d8
omaaarsh <omarsherifelghamry@gmail.com>
2026-02-16 04:28:15 +0200

    Fix config validation for Heroku production

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -8,12 +8,35 @@ export interface ConfigType{
    auth: AuthConfig;
 }
 export const appConfigSchema = Joi.object({
-    APP_MESSAGE_PREFIX: Joi.string().default('Hello'),
-    DB_HOST: Joi.string().default('localhost'),
-    DB_PORT: Joi.number().default(5432),
-    DB_USERNAME: Joi.string().required(),
-    DB_PASSWORD: Joi.string().required(),
-    DB_NAME: Joi.string().required(),
-    JWT_TOKEN: Joi.string().required(),
-    JWT_EXPIRES_IN: Joi.string().required(),
-})
+  APP_MESSAGE_PREFIX: Joi.string().default('Hello'),
+
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(5432),
+
+  DB_USERNAME: Joi.when('NODE_ENV', {
+    is: 'production',
+    then: Joi.optional(),
+    otherwise: Joi.string().required(),
+  }),
+
+  DB_PASSWORD: Joi.when('NODE_ENV', {
+    is: 'production',
+    then: Joi.optional(),
+    otherwise: Joi.string().required(),
+  }),
+
+  DB_NAME: Joi.when('NODE_ENV', {
+    is: 'production',
+    then: Joi.optional(),
+    otherwise: Joi.string().required(),
+  }),
+
+  DATABASE_URL: Joi.string().when('NODE_ENV', {
+    is: 'production',
+    then: Joi.required(),
+    otherwise: Joi.optional(),
+  }),
+
+  JWT_TOKEN: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required(),
+});
